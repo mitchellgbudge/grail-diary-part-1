d0c4a8ca40a876768071f3ac9441ee8946ff96e1
Mitchell Budge <mitchellbudge@gmail.com>
2019-08-13 13:38:29 -0600

    Configure UI on Main.storyboard

---
+++ GrailDiary/GrailDiary/CluesTableViewController.swift
@@ -0,0 +1,20 @@
+//
+//  CluesTableViewController.swift
+//  GrailDiary
+//
+//  Created by Mitchell Budge on 8/13/19.
+//  Copyright © 2019 Mitchell Budge. All rights reserved.
+//
+
+import UIKit
+
+class CluesTableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
